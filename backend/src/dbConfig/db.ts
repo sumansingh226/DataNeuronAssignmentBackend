84752c23f8b523afd3d3101d41f0a118e607392e
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-20 22:54:01 +0530

    adding Item Model

--- backend/src/dbConfig/db.ts
+++ backend/src/dbConfig/db.ts
@@ -1,19 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 require("dotenv").config();
 
 export const connectDb = async () => {
     const dbURI: string = process.env.CONNECTION_URL ?? "";
     try {
-        const client: any = await mongoose.connect(dbURI, {
+        await mongoose.connect(dbURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         } as any);
-
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
 };
-
-
-
