6591133d91dc4f9f89b7f2b002363ece02d3f6c4
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-20 23:11:34 +0530

    adding Item Model and formatting code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,7 @@
-// app.ts
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDb } from './dbConfig/db';
-import itemsRouter from './routes/itemRoutes';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./dbConfig/db";
+import itemsRouter from "./routes/itemRoutes";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,34 +13,33 @@ const app = express();
 app.use(express.json());
 
 // Mount the itemsRouter at the '/items' path
-app.use('/items', itemsRouter);
+app.use("/items", itemsRouter);
 
 // Define the port to listen on, defaulting to 50000 if not specified in the environment
 const PORT = process.env.PORT || 50000;
 
 // Connect to the database and start the server
-connectDb().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port http://localhost:${PORT}`);
-    });
-});
+const startServer = async () => {
+    try {
+        await connectDb();
+        app.listen(PORT, () => {
+            console.log(`Server running on port http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
 
+// Start the server
+startServer();
 
-/**
- * Handle uncaught exceptions and unhandled rejections in a Node.js application.
- * When an uncaught exception or unhandled rejection occurs, log the error and exit the process.
- */
-
-// Handle uncaught exceptions
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
+// Handle uncaught exceptions and unhandled rejections
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception:", err);
     process.exit(1);
 });
 
-// Handle unhandled rejections
-process.on('unhandledRejection', (err) => {
-    console.error('Unhandled Rejection:', err);
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection:", err);
     process.exit(1);
 });
-
-
