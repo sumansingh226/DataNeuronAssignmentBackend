6a565ce9e33cfaa5c1f0622e343f9f0bcfa5bc42
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-20 23:01:47 +0530

    adding Item Model

--- backend/src/dbConfig/db.ts
+++ backend/src/dbConfig/db.ts
@@ -1,15 +1,24 @@
 import mongoose from "mongoose";
 require("dotenv").config();
 
-export const connectDb = async () => {
-    const dbURI: string = process.env.CONNECTION_URL ?? "";
+/**
+ * Connects to the MongoDB database using the provided connection URL.
+ * @returns {Promise<void>} A promise that resolves when the connection is successful.
+ */
+export const connectDb = async (): Promise<void> => {
+    // Retrieve the database connection URL from the environment variables
+    const dbURI: string = process.env.CONNECTION_URL || "";
     try {
+        // Connect to the MongoDB database
         await mongoose.connect(dbURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         } as any);
+        // Log a message indicating a successful connection
         console.log("Connected to MongoDB");
     } catch (error) {
+        // Log an error message if the connection fails
         console.error("Error connecting to MongoDB:", error);
     }
 };
+
