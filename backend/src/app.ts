cd7cd4de1dcd9590e3c436f5798677a7beee0df2
sumansingh226 <sumansingh13062000@gmail.com>
2024-02-21 02:16:21 +0530

    define dumcntation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,8 +12,15 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Welcome! to express....");
+});
+
 // Mount the itemsRouter at the '/items' path
 app.use("/items", itemsRouter);
+
 
 // Define the port to listen on, defaulting to 50000 if not specified in the environment
 const PORT = process.env.PORT || 5000;
